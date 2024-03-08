c7b9e2ceb9711c77fbfb0928631c2558ab3276f6
polina <you@example.com>
2024-03-08 14:40:39 +0300

    done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Automate-looking-for-the-best-price
+# Automate-looking-for-the-best-price  
+
+This program is a price scraper.
+
+You shoul add a link to the product you want for a cheaper price, your email login and password and also the price that you're looking for.
+
+Every day this program scrapes the store page to see if the product price is equeal or less that you wanted.
+If it's so you'll got an email that informs you about the price reduction 
+
+As an example I've used the lamoda store link
---
+++ main.py
@@ -0,0 +1,70 @@
+import requests, schedule, time, os, smtplib
+from bs4 import BeautifulSoup
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+password = "yttlukncjxinzgoe"
+username = "polina.zolotova.qr@gmail.com"
+
+data = {}
+
+url = "https://www.lamoda.ru/p/mp002xw0yzrn/shoes-remonte-botinki/"
+data["price_wanted"] = 6500
+
+def scrape():
+    try:
+        response = requests.get(url)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        prices = soup.find_all("span", {"class": "x-premium-product-prices__price"})
+        good_price = False
+        price = {}
+        
+        if len(prices) == 1:
+            price = prices[0]
+        elif len(prices) == 2:
+            price = prices[1]
+        elif len(prices) == 0: # no price at all - item out of stock
+            return good_price 
+        
+        data["price_now"] = int(float(price["content"]))
+        
+        if data["price_wanted"] >= data["price_now"]:
+            good_price = True
+
+        print(data["price_now"])
+        
+
+    except Exception as err: # request returns error
+        print(err) 
+        
+    return good_price
+
+
+def send_mail():
+    if not scrape():
+        return
+        
+    email = f"""<p><a href='{url}'> This item </a> is now {data['price_now']} and you wanted to buy in at least for {data['price_wanted']} </p>"""
+    
+    server = "smtp.gmail.com"
+    port = 587
+    s = smtplib.SMTP(host=server, port=port)
+    s.starttls()
+    s.login(username, password)
+    
+    msg = MIMEMultipart()
+    msg["To"] = username
+    msg["From"] = username
+    msg["Subject"] = "Good chance to buy some stuff"
+    msg.attach(MIMEText (email, "html"))
+    s.send_message(msg)
+    del(msg)
+    print(f"Email notification has been sent. Price now is {data['price_now']}. Price that you wanted is {data['price_wanted']}.")
+
+
+schedule.every().day.at("12:00").do(send_mail)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
